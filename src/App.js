56fdc8940dc47e481f5cc8a26196e86b1575d79f
Lucas Veras <veraslucas669@gmail.com>
2021-10-16 10:53:40 -0300

    Tela de cadastro, atualizada

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import RegistrationPage from './RegistrationPage'
-import AddressRegistrationPage from './AddressRegistrationPage';
+
 
 function App() {
   return (
     <div>
-     <RegistrationPage/>
-     {/* <AddressRegistrationPage/> */}
+  
     </div>
   );
 }
